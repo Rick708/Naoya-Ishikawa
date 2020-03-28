ecd050f9f5cccff1b300b6079a824628e7586047
Rick708 <Rick708@users.noreply.github.com>
2020-03-28 15:37:26 +0900

    Scenesの作成と画像

---
+++ Assets/Script/Game/GameSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSystem : MonoBehaviour
+{
+    public GameObject Mole;
+    void Start()
+    {
+
+    }
+
+    public void OnStartButton()
+    {
+        EncountMole();
+    }
+
+    void EncountMole()
+    {
+        Debug.Log("mogura");
+        Instantiate(Mole);
+    }
+
+    //ステージ変更
+    public void OnTitleButton()
+    {
+        SceneManager.LoadScene("Title");// 指定したシーンに移動
+    }
+    public void OnHistoryButton()
+    {
+        SceneManager.LoadScene("History");
+    }
+}
