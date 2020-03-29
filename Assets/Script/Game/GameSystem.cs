38d639e3e1797d290b350480d695646f86dda3c1
Rick708 <Rick708@users.noreply.github.com>
2020-03-29 17:31:02 +0900

    ぷsh

--- Assets/Script/Game/GameSystem.cs
+++ Assets/Script/Game/GameSystem.cs
@@ -5,22 +5,54 @@ using UnityEngine.SceneManagement;
 
 public class GameSystem : MonoBehaviour
 {
+
     public GameObject Mole;
+    public GameObject TitleButton;
+    public GameObject GameButton;
+    public GameObject HistoryButton;
+    MoleManager moleManager;
+
+    //初期設定
     void Start()
     {
 
+        TitleButton.SetActive(false);
+        GameButton.SetActive(false);
+        HistoryButton.SetActive(false);
+        //moleManager.AddEventListenerOnTap(MoleTap);
     }
 
+    //ゲームスタート
     public void OnStartButton()
     {
+        //制限時間スタート
         EncountMole();
     }
-
+    //モグラ遭遇
     void EncountMole()
     {
-        Debug.Log("mogura");
-        Instantiate(Mole);
+        //３体を場所固定のランダム生成したい。
+        //最終的には確率でレアモグラも生成
+        Mole = Instantiate(Mole);
+
     }
+    //タップされた時のアクション
+    //画像を消す、点数をつける
+
+    public void 
+
+    void MoleTap(MoleManager moleManager)
+    {
+        moleManager.AddEventListenerOnTap(MoleTap);
+        Debug.Log("いけた！！");
+        Destroy(Mole.gameObject);
+    }
+
+
+
+
+
+
 
     //ステージ変更
     public void OnTitleButton()
@@ -30,5 +62,9 @@ public class GameSystem : MonoBehaviour
     public void OnHistoryButton()
     {
         SceneManager.LoadScene("History");
+    }
+    public void OnGameButton()
+    {
+        //ゲームのスタート画面に戻りたい。
     }
 }
---
+++ Assets/Script/Game/MoleManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class MoleManager : MonoBehaviour
+{
+    Action tapAction;
+
+    public void AddEventListenerOnTap(Action action)
+    {
+        tapAction += action;
+    }
+
+    public void OnTap()
+    {
+        Debug.Log("クリックされた");
+        tapAction();
+    }
+}
